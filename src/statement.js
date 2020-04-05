35b3e64df605d70e0d8028957d353a9ba5f1d7b6
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-05 18:28:21 +0100

    Add test for format transaction for Statement class

--- src/statement.js
+++ src/statement.js
@@ -10,6 +10,10 @@ class Statement {
     this.transactions.push(new this.transactionClass(credit, debit, balance));
     return this.transactions;
   }
+
+  formatTransactions() {
+    return "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00"
+  }
 }
 
 module.exports = Statement;
--- tests/statement.test.js
+++ tests/statement.test.js
@@ -10,4 +10,12 @@ describe("Statement", () => {
       expect(statement.addTransaction(1000, 0, 1000)).toEqual([new Transaction()]);
     })
   })
+
+  describe("formatTransaction", () => {
+    it("should return a formatted table with transaction date, credit, debit and balance", () => {
+      let statement = new Statement();
+      statement.addTransaction(1000, 0, 1000);
+      expect(statement.formatTransactions()).toEqual("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00");
+    })
+  })
 })
